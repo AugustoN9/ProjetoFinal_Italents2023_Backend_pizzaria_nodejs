0a436c40fcab483e90aaae992a4e2f23e363604d
AugustoGit <auguston8@hotmail.com>
2023-06-13 22:41:48 -0300

    Fix: concluido do pedido

--- src/router/categoria.router.js
+++ src/router/categoria.router.js
@@ -7,10 +7,8 @@ const { validaCategoria } = require("../middleware/validacao.middleware");
 router.get("/findAll", authMiddleware, categoriaController.findAllCategoryController);
 router.get("/findById/:id", authMiddleware, categoriaController.findByIdCategoryController);
 
-const { validaCategoria } = require("../middleware/validacao.middleware");
 router.post("/create", authMiddleware, validaCategoria, categoriaController.createCategoryController);
 
-const { validaCategoria } = require("../middleware/validacao.middleware");
 router.put("/update/:id", authMiddleware, validaCategoria, categoriaController.updateCategoryController);
 
 router.delete("/delete/:id", authMiddleware, categoriaController.removeCategoryController);
